9c8229da77f846831d1f8bf1b267d744e0950b67
fagyottpatko <mihalybalogh06@gmail.com>
2026-04-18 17:03:29 +0200

    Search fixed

--- search.php
+++ search.php
@@ -3,15 +3,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $query = urlencode($_POST["query"]);
     $apiKey = " ";
-
-    $maxArticles = 20;
+    $maxArticles = 35;
 
     $url = "https://newsapi.org/v2/everything?q=$query&pageSize=$maxArticles&apiKey=$apiKey";
 
     $ch = curl_init($url);
-
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
         "User-Agent: MyCyberpunkApp/1.0"
     ]);
@@ -22,16 +19,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $data = json_decode($response, true);
 
     $articles = [];
-
-    if (isset($data["articles"])) {
+    if (isset($data["articles"]) && is_array($data["articles"])) {
         foreach ($data["articles"] as $article) {
             $articles[] = [
-                "title" => $article["title"] ?? "No title",
-                "desc" => $article["description"] ?? "No description"
+                "title" => !empty($article["title"]) ? $article["title"] : "No title",
+                "desc" => !empty($article["description"]) ? $article["description"] : "No description",
+                "url" => !empty($article["url"]) ? $article["url"] : "#",
+                "image" => !empty($article["urlToImage"]) ? $article["urlToImage"] : "",
+                "source" => isset($article["source"]["name"]) ? $article["source"]["name"] : "Unknown",
+                "date" => !empty($article["publishedAt"]) ? $article["publishedAt"] : ""
             ];
         }
     }
-
+    header('Content-Type: application/json');
     echo json_encode(["articles" => $articles]);
 }
 ?>
